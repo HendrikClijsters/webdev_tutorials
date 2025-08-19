deeac7cdd5ac32116486b77f6dfcae8f45a6c4c2
Hendrik Clijsters <hendrik.clijsters@mosa-rt.be>
2025-08-19 19:14:08 +0200

    django_beginner_cbi -- Episode 4

--- django_beginner_CBI/posts/views.py
+++ django_beginner_CBI/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    title = "This is my title."
+    context = {'title':title}
+    return render(request, 'posts/home.html', context)
+
+def blogposts(request):
+    context = {}
+    return render(request, 'posts/blogposts.html', context)
